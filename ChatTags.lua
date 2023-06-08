0118e1d19acbf70fc16ce2323d0d1e0c7ae54072
sdhhf <95890805+sdhhf1245@users.noreply.github.com>
2023-06-08 11:16:05 -0700

    vape v4

---
+++ ChatTags.lua
@@ -0,0 +1,70 @@
+return {
+	[""] = {
+		TagText = "Soggy",
+		TagColor = Color3.new(0.7, 0, 1)
+	},
+	[""] = {
+		TagText = "VAPE PRIVATE",
+		TagColor = Color3.new(0.7, 0, 1)
+	},
+	[""] = {
+		TagText = "CO-RAY OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["RayFaxiu"] = {
+		TagText = "RAY MOBILE",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["ChaseBuiltLikePringl"] = {
+		TagText = "Soggy",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["IdkDudeWhatYou"] = {
+		TagText = "RAY MAIN",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	[""] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["table0x768"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["XylexIsCanadain"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["table0x472"] = {
+		TagText = "RISE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	[""] = {
+		TagText = "RAYWARE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	[""] = {
+		TagText = "RAYWARE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+  	["MannDigs"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["Y"] = {
+		TagText = "COOL MAN",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["table5x87c7a589vt2f"] = {
+		TagText = "VAPE OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	},
+	["ImABlackNatzu"] = {
+		TagText = "Mont Owner",
+		TagColor = Color3.new(0.7, 0, 1)
+	},
+	["MontWFrFr"] = {
+		TagText = "MONT OWNER",
+		TagColor = Color3.fromRGB(255,95,95)
+	}
+}
